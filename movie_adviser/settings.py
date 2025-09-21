4ee01fc920f1caf29c7ee519c5dbda492848d5a0
maksym <1maksymrusanov1@gmail.com>
2025-09-21 21:25:42 +0000

    change settings file

--- movie_adviser/settings.py
+++ movie_adviser/settings.py
@@ -5,7 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = [
+    "*", "djangopractice-production.up.railway.app", "127.0.0.1", "localhost"]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
